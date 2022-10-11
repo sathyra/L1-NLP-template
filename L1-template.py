a13caece9022330ea56938fde13dfeda0141724b
sathyra <105861021+sathyra@users.noreply.github.com>
2022-10-11 15:33:57 +0530

    Add files via upload

---
+++ L1-template.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from pytesseract import pytesseract
+
+# Defining paths to tesseract.exe
+# and the image we would be using
+
+# Opening the image & storing it in an image object
+
+# Providing the tesseract executable
+# location to pytesseract library
+
+
+# Passing the image object to image_to_string() function
+# This function will extract the text from the image
+
+# Displaying the extracted text
+
